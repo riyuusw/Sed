8f8a5f6b586b1f59de6ff52fda926d3795c05712
riyuusw <100389715+riyuusw@users.noreply.github.com>
2024-09-23 13:29:47 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,8 @@ url_upgrade_storage = 'https://elb.seeddao.org/api/v1/seed/storage-size/upgrade'
 url_upgrade_mining = 'https://elb.seeddao.org/api/v1/seed/mining-speed/upgrade'
 url_upgrade_holy = 'https://elb.seeddao.org/api/v1/upgrades/holy-water'
 url_get_profile = 'https://elb.seeddao.org/api/v1/profile'
-url_worms = 'https://elb.seeddao.org/api/v1/worms'
+
+# Headers yang diperlukan untuk request
 headers = {
     'accept': 'application/json, text/plain, */*',
     'accept-language': 'en-ID,en-US;q=0.9,en;q=0.8,id;q=0.7',
@@ -62,62 +63,72 @@ def load_credentials():
         return []
 
 def check_worm():
-    response = requests.get(url_worms, headers=headers)
+    response = requests.get('https://elb.seeddao.org/api/v1/worms', headers=headers)
     if response.status_code == 200:
         worm_data = response.json()['data']
-        next_refresh = worm_data.get('next_refresh')
-        is_caught = worm_data.get('is_caught')
+        next_refresh = worm_data['next_refresh']
+        is_caught = worm_data['is_caught']
 
-        if next_refresh:
-            next_refresh_dt = datetime.datetime.fromisoformat(next_refresh[:-1] + '+00:00')
-            now_utc = datetime.datetime.now(pytz.utc)
-            time_diff_seconds = (next_refresh_dt - now_utc).total_seconds()
-            hours = int(time_diff_seconds // 3600)
-            minutes = int((time_diff_seconds % 3600) // 60)
-            print(f"{Fore.GREEN+Style.BRIGHT}[ Worms ]: Next in {hours} jam {minutes} menit - Status: {'Caught' if is_caught else 'Available'}")
-        else:
-            print(f"{Fore.RED+Style.BRIGHT}[ Worms ]: Data 'next_refresh' tidak tersedia.")
-        
+        next_refresh_dt = datetime.datetime.fromisoformat(next_refresh[:-1] + '+00:00')
+        now_utc = datetime.datetime.now(pytz.utc)
+
+        time_diff_seconds = (next_refresh_dt - now_utc).total_seconds()
+        hours = int(time_diff_seconds // 3600)
+        minutes = int((time_diff_seconds % 3600) // 60)
+
+        print(f"{Fore.GREEN + Style.BRIGHT}[ Worms ]: Next in {hours} jam {minutes} menit - Status: {'Caught' if is_caught else 'Available'}")
+
         return worm_data
     else:
-        print(f"{Fore.RED+Style.BRIGHT}[ Worms ]: Gagal mendapatkan data worm.")
+        print(f"{Fore.RED + Style.BRIGHT}[ Worms ]: Gagal mendapatkan data worm.")
         return None
 
 def catch_worm():
     worm_data = check_worm()
-    if worm_data and not worm_data.get('is_caught'):
+    if worm_data and not worm_data['is_caught']:
         response = requests.post('https://elb.seeddao.org/api/v1/worms/catch', headers=headers)
         if response.status_code == 200:
-            print(f"{Fore.GREEN+Style.BRIGHT}[ Worms ]: Berhasil menangkap")
+            print(f"{Fore.GREEN + Style.BRIGHT}[ Worms ]: Berhasil menangkap")
         elif response.status_code == 400:
-            print(f"{Fore.RED+Style.BRIGHT}[ Worms ]: Sudah terangkap")
+            print(f"{Fore.RED + Style.BRIGHT}[ Worms ]: Sudah tertangkap")
         elif response.status_code == 404:
-            print(f"{Fore.RED+Style.BRIGHT}[ Worms ]: Worm tidak ditemukan")
+            print(f"{Fore.RED + Style.BRIGHT}[ Worms ]: Worm tidak ditemukan")    
         else:
-            print(f"{Fore.RED+Style.BRIGHT}[ Worms ]: Gagal menangkap worm, status code:", response.status_code)
+            print(f"{Fore.RED + Style.BRIGHT}[ Worms ]: Gagal menangkap worm, status code:", response)
     else:
-        print(f"{Fore.RED+Style.BRIGHT}[ Worms ]: Worm tidak tersedia atau sudah tertangkap.")
+        print(f"{Fore.RED + Style.BRIGHT}[ Worms ]: Worm tidak tersedia atau sudah tertangkap.")
 
 def get_profile():
     response = requests.get(url_get_profile, headers=headers)
     if response.status_code == 200:
         profile_data = response.json()
         name = profile_data['data']['name']
-        print(f"{Fore.CYAN+Style.BRIGHT}============== [ Akun | {name} ] ==============")
-        upgrades = {upgrade['upgrade_type']: upgrade['upgrade_level'] for upgrade in profile_data['data']['upgrades']}
+        print(f"{Fore.CYAN + Style.BRIGHT}============== [ Akun | {name} ] ==============")
+
+        upgrades = {}
+        for upgrade in profile_data['data']['upgrades']:
+            upgrade_type = upgrade['upgrade_type']
+            upgrade_level = upgrade['upgrade_level']
+            if upgrade_type in upgrades:
+                if upgrade_level > upgrades[upgrade_type]:
+                    upgrades[upgrade_type] = upgrade_level
+            else:
+                upgrades[upgrade_type] = upgrade_level
+
         for upgrade_type, level in upgrades.items():
-            print(f"{Fore.BLUE+Style.BRIGHT}[ {upgrade_type.capitalize()} Level ]: {level + 1}")
+            print(f"{Fore.BLUE + Style.BRIGHT}[ {upgrade_type.capitalize()} Level ]: {level + 1}")
     else:
         print("Gagal mendapatkan data, status code:", response.status_code)
+        return None
 
 def check_balance():
     response = requests.get(url_balance, headers=headers)
     if response.status_code == 200:
         balance_data = response.json()
-        print(f"{Fore.YELLOW+Style.BRIGHT}[ Balance ]: {balance_data['data'] / 1000000000}")
+        print(f"{Fore.YELLOW + Style.BRIGHT}[ Balance ]: {balance_data['data'] / 1000000000}")
         return True
     else:
-        print(f"{Fore.RED+Style.BRIGHT}[ Balance ]: Gagal | {response.status_code}")
+        print(f"{Fore.RED + Style.BRIGHT}[ Balance ]: Gagal |{response.status_code}")
         return False
 
 def cekin_daily():
@@ -125,33 +136,45 @@ def cekin_daily():
     if response.status_code == 200:
         data = response.json()
         day = data.get('data', {}).get('no', '')
-        print(f"{Fore.GREEN+Style.BRIGHT}[ Check-in ]: Check-in berhasil | Day {day}")
+        print(f"{Fore.GREEN + Style.BRIGHT}[ Check-in ]: Check-in berhasil | Day {day}")
     else:
         data = response.json()
         if data.get('message') == 'already claimed for today':
-            print(f"{Fore.RED+Style.BRIGHT}[ Check-in ]: Sudah dilakukan hari ini")
+            print(f"{Fore.RED + Style.BRIGHT}[ Check-in ]: Sudah dilakukan hari ini")
         else:
-            print(f"{Fore.RED+Style.BRIGHT}[ Check-in ]: Gagal | {data}")
+            print(f"{Fore.RED + Style.BRIGHT}[ Check-in ]: Gagal | {data}")
 
-def upgrade(storage_confirm, mining_confirm, holy_confirm):
-    upgrades = {
-        'storage': (url_upgrade_storage, storage_confirm),
-        'mining': (url_upgrade_mining, mining_confirm),
-        'holy': (url_upgrade_holy, holy_confirm)
-    }
-    results = []
-    for upgrade_type, (url, confirm) in upgrades.items():
-        if confirm.lower() == 'y':
-            response = requests.post(url, headers=headers)
-            if response.status_code == 200:
-                results.append(f'[ Upgrade {upgrade_type} ]: Berhasil')
-            else:
-                results.append(f'[ Upgrade {upgrade_type} ]: Balance tidak tercukupi')
-    return results
+def upgrade_storage(confirm):
+    if confirm.lower() == 'y':
+        response = requests.post(url_upgrade_storage, headers=headers)
+        if response.status_code == 200:
+            return '[ Upgrade storage ]: Berhasil'
+        else:
+            return '[ Upgrade storage ]: Balance tidak tercukupi'
+    return None
+
+def upgrade_mining(confirm):
+    if confirm.lower() == 'y':
+        response = requests.post(url_upgrade_mining, headers=headers)
+        if response.status_code == 200:
+            return '[ Upgrade mining ]: Berhasil'
+        else:
+            return '[ Upgrade mining ]: Balance tidak tercukupi'
+    return None
+
+def upgrade_holy(confirm):
+    if confirm.lower() == 'y':
+        response = requests.post(url_upgrade_holy, headers=headers)
+        if response.status_code == 200:
+            return '[ Upgrade holy ]: Berhasil'
+        else:
+            return '[ Upgrade holy ]: Syarat tidak terpenuhi'
+    return None
 
 def get_tasks():
     response = requests.get('https://elb.seeddao.org/api/v1/tasks/progresses', headers=headers)
     tasks = response.json()['data']
+    
     for task in tasks:
         if task['task_user'] is None or not task['task_user']['completed']:
             complete_task(task['id'], task['name'])
@@ -159,45 +182,67 @@ def get_tasks():
 def complete_task(task_id, task_name):
     response = requests.post(f'https://elb.seeddao.org/api/v1/tasks/{task_id}', headers=headers)
     if response.status_code == 200:
-        print(f"{Fore.GREEN+Style.BRIGHT}[ Tasks ]: Tugas {task_name} selesai.")
+        print(f"{Fore.GREEN + Style.BRIGHT}[ Tasks ]: Tugas {task_name} selesai.")
     else:
-        print(f"{Fore.RED+Style.BRIGHT}[ Tasks ]: Gagal menyelesaikan tugas {task_name}, status code: {response.status_code}")
+        print(f"{Fore.RED + Style.BRIGHT}[ Tasks ]: Gagal menyelesaikan tugas {task_name}, status code: {response.status_code}")
 
 def main():
     print_welcome_message()
-    tokens = load_credentials()
-    storage_confirm = input("Auto upgrade storage? (y/n): ")
-    mining_confirm = input("Auto upgrade mining? (y/n): ")
-    holy_confirm = input("Auto upgrade holy? (y/n): ")
-    task_confirm = input("Auto Clear Task? (y/n): ")
+    tokens = load_credentials()  # Memuat daftar token
+    
+    confirm_storage = input("Auto upgrade storage? (y/n): ")
+    confirm_mining = input("Auto upgrade mining? (y/n): ")
+    confirm_holy = input("Auto upgrade holy? (y/n): ")
+    confirm_task = input("Auto Clear Task? (y/n): ")
 
     while True:
         for index, token in enumerate(tokens):
             headers['telegram-data'] = token
-            get_profile()
-            hasil_upgrade = upgrade(storage_confirm, mining_confirm, holy_confirm)
-
-            for result in hasil_upgrade:
-                print(result)
+            info = get_profile()
+            if info:
+                print(f"Memproses untuk token ke {info['data']['name']}")
+                
+            hasil_upgrade = upgrade_storage(confirm_storage)
+            hasil_upgrade1 = upgrade_mining(confirm_mining)
+            hasil_upgrade2 = upgrade_holy(confirm_holy)
+            
+            if hasil_upgrade:
+                print(hasil_upgrade)
+                time.sleep(1)  
+            if hasil_upgrade1:
+                print(hasil_upgrade1) 
+                time.sleep(1)
+            if hasil_upgrade2:
+                print(hasil_upgrade2)
                 time.sleep(1)
 
+            # Melakukan GET request untuk cek balance terlebih dahulu
             if check_balance():
+                # Jika berhasil mendapatkan balance, lakukan POST request untuk claim
                 response = requests.post(url_claim, headers=headers)
+
+                # Cek status code dari response
                 if response.status_code == 200:
-                    print(f"{Fore.GREEN+Style.BRIGHT}[ Claim ]: Claim berhasil")
+                    print(f"{Fore.GREEN + Style.BRIGHT}[ Claim ]: Claim berhasil")
                 elif response.status_code == 400:
-                    print(f"{Fore.RED+Style.BRIGHT}[ Claim ]: Belum waktunya claim")
+                    print(f"{Fore.RED + Style.BRIGHT}[ Claim ]: Belum waktunya claim")
+                else:
+                    print("Terjadi kesalahan, status code:", response.status_code)
+
                 cekin_daily()
                 catch_worm()
-                if task_confirm.lower() == 'y':
+                if confirm_task.lower() == 'y':
                     get_tasks()
-                    
-                for i in range(30 * 60, 0, -1):  # 30 menit dalam detik
-                        sys.stdout.write(f"\r{Fore.CYAN+Style.BRIGHT}============ Selesai, tunggu {i // 60} menit {i % 60} detik.. ============")
-                        sys.stdout.flush()
-                        time.sleep(1)
-                        print()  # Cetak baris baru setelah hitungan mundur selesai
-                        clear_console()
+
+        # Tunggu 30 menit setelah semua akun diproses
+        for i in range(30 * 60, 0, -1):  # 30 menit dalam detik
+            sys.stdout.write(f"\r{Fore.CYAN + Style.BRIGHT}============ Selesai, tunggu {i // 60} menit {i % 60} detik.. ============")
+            sys.stdout.flush()
+            time.sleep(1)
+        print()  # Cetak baris baru setelah hitungan mundur selesai
+
+        # Membersihkan konsol setelah hitungan mundur
+        clear_console()
 
 if __name__ == "__main__":
     main()
